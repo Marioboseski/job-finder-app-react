d152f119b0a36d6ff3c2dbca90a0012c9871ac3e
Mario Boseski <crboseskimario@gmail.com>
2025-12-14 18:18:52 +0100

    Add login-form page in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import Home from "./pages/home/Home";
 import Jobs from "./pages/jobs/Jobs";
 import Bookmarks from "./pages/bookmarks/Bookmarks";
 import Profile from "./pages/profile/Profile";
+import LoginForm from "./components/login-form/LoginForm";
 import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<LoginForm />}/>
+        <Route path="/home" element={<Home />} />
         <Route path="/jobs" element={<Jobs />} />
         <Route path="/bookmarks" element={<Bookmarks />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
