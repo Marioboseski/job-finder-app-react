c1869d5d93935944f8d8b9b0aa0b3be5ac5bed1d
Mario Boseski <crboseskimario@gmail.com>
2025-12-21 18:27:30 +0100

    Add media queries in Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -16,18 +16,20 @@ const Home = () => {
   return (
     <section className="home-section">
       <div className="container">
-        <div className="home-wrapper">
-          <div className="home-content">
-            <h1>Find your dream job</h1>
-            <p className="sub-heading">Search and apply for your dream job today</p>
-            <button onClick={handleNavigate}>Browse jobs</button>
-            <img src={homeimg} alt="home-image" />
-            <p className="user-trust-paragraph">Trusted by over 200.000 users</p>
-            <div className="users-logos">
-              <img src={airbnb} alt="google-logo" />
-              <img src={google} alt="google-logo" />
-              <img src={facebook} alt="google-logo" />
-              <img src={microsoft} alt="google-logo" />
+        <div className="home-layout">
+          <div className="home-wrapper">
+            <div className="home-content">
+              <h1>Find your dream job</h1>
+              <p className="sub-heading">Search and apply for your dream job today</p>
+              <button onClick={handleNavigate}>Browse jobs</button>
+              <img src={homeimg} alt="home-image" />
+              <p className="user-trust-paragraph">Trusted by over 200.000 users</p>
+              <div className="users-logos">
+                <img src={airbnb} alt="google-logo" />
+                <img src={google} alt="google-logo" />
+                <img src={facebook} alt="google-logo" />
+                <img src={microsoft} alt="google-logo" />
+              </div>
             </div>
           </div>
         </div>
